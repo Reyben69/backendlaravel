fb8cc679c9f40e1bec7351ea1db309416f66075c
Reyben69 <pajuta.reybenedic@cec.edu.ph>
2025-05-14 14:18:47 +0800

    WIP: Save local changes before rebase

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;git branch -m main
+
+Route::post('register1',[AuthController::class,'store']);
+Route::post('logn1',[AuthController::class,'login']);
+
+
+
+
+
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
